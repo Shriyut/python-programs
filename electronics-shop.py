d83d02bb90d4b3e91581ae118dbeabb153e7d7c1
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2020-04-08 08:05:47 +0530

    Create electronics-shop.py

---
+++ electronics-shop.py
@@ -0,0 +1,52 @@
+#https://www.hackerrank.com/challenges/electronics-shop/problem
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    sp = []
+    index = 0
+    for i in range(0, len(keyboards)):
+        for j in range(0, len(drives)):
+            if (keyboards[i]+drives[j] <= b):
+                sp.append(keyboards[i]+drives[j])
+                index = index+1
+    
+    if (len(sp) == 0):
+        return -1
+    else:
+        return max(sp)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
